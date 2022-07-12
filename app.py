5ce9d3fe49733decd194e4a77e08620db1a88ce4
jick2 <jicortes1@miuandes.cl>
2022-07-12 09:36:50 -0700

    feat(wsgi)

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__, static_url_path='', static_folder="frontend/out")
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 CORS(app)
 user_controller = UsersView()
-
+wsgi_app = app.wsgi_app
 register_controller = RegistersView()
 register_controller.register(app)
 user_controller.register(app)
